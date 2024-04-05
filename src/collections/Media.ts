6d9d32bd006b3a84634bec28cb8baf78fedfa0dd
J <josephmonge00@gmail.com>
2024-04-04 17:53:17 -0700

    Create Product and Media collections

---
+++ src/collections/Media.ts
@@ -0,0 +1,89 @@
+import { User } from "../payload-types";
+import { Access, CollectionConfig } from "payload/types";
+
+// returns an access policy
+const isAdminOrHasAccessToImages =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    // allow access to your images
+    return {
+      // image's user field
+      user: {
+        equals: req.user.id,
+      },
+    };
+  };
+
+export const Media: CollectionConfig = {
+  slug: "media",
+  hooks: {
+    beforeChange: [
+      ({ req, data }) => {
+        return { ...data, user: req.user.id };
+      },
+    ],
+  },
+  access: {
+    // people browsing the store should be able to see all images, but not sellers from the backend
+    read: async ({ req }) => {
+      const referer = req.headers.referer;
+
+      if (!req.user || !referer?.includes("sell")) {
+        return true;
+      }
+
+      // TODO: understand
+      return await isAdminOrHasAccessToImages()({ req });
+    },
+    // isAdmin... func looks weird above there to look pretty here
+    update: isAdminOrHasAccessToImages(),
+    delete: isAdminOrHasAccessToImages(),
+  },
+  admin: {
+    // hides Media from sidebar
+    hidden: ({ user }) => user.role !== "admin",
+  },
+  upload: {
+    staticURL: "/media",
+    staticDir: "media",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: 300,
+        position: "centre",
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre",
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        //retains aspects ratio, calculate height automatically
+        height: undefined,
+        position: "centre",
+      },
+    ],
+    mimeTypes: ["image/*"],
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+  ],
+};
---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,118 @@
+import { PRODUCT_CATEGORIES } from "../../config";
+import { CollectionConfig } from "payload/types";
+
+export const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {},
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      // hides this field from admin dashboard
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: "name",
+      label: "Name",
+      type: "textarea",
+      required: true,
+    },
+    {
+      name: "description",
+      label: "Product details",
+      type: "text",
+    },
+    {
+      name: "price",
+      label: "Price (USD)",
+      type: "number",
+      min: 10,
+      max: 1000,
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product status",
+      type: "select",
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      defaultValue: "pending",
+      options: [
+        {
+          label: "Pending verification",
+          value: "pending",
+        },
+        {
+          label: "Approved",
+          value: "approved",
+        },
+        {
+          label: "Denied",
+          value: "denied",
+        },
+      ],
+    },
+    {
+      name: "priceId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "stripeId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "images",
+      label: "Product images",
+      type: "array",
+      minRows: 1,
+      maxRows: 1,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
