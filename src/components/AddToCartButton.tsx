6f9eb7e3b0a05045978e6e6ccfa6b2b8da9a7d2b
J <josephmonge00@gmail.com>
2024-04-06 14:18:05 -0700

    Create product page template

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { useCart } from "@/hooks/useCart";
+
+const AddToCartButton = () => {
+  const { addItem } = useCart();
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [isSuccess]);
+
+  return (
+    <Button
+      onClick={(e) => {
+        setIsSuccess(true);
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? "Added!" : "Add to cart"}
+    </Button>
+  );
+};
+
+export default AddToCartButton;
